c2f6c1554fc21c8a1a1020ff71c6fb766b38ef01
CN-Scars <yss2813483011xxl@gmail.com>
2024-05-21 17:04:25 +0800

    Completed the basic structure design of AboutMe.vue

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,23 +1,56 @@
 <template>
-    <div class="about-me">
-        <h1>About Me</h1>
-    </div>
+    <v-container class="root fill-height" fluid>
+        <v-row align="center" justify="center" class="text-center">
+            <v-col cols="12">
+                <v-img id="avatar" src="@/assets/scars_logo.png" max-width="150" aspect-ratio="1" class="mx-auto"
+                    contain></v-img>
+            </v-col>
+            <v-col cols="12">
+                <h1 class="name">Scars</h1>
+            </v-col>
+            <v-col cols="12">
+                <div class="subTitle">
+                    Scars的简介
+                </div>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script>
 export default {
-    name: 'AboutMe',
+    name: "AboutMe",
 };
 </script>
 
 <style scoped>
-.about-me {
-    background-color: #f44336;
-    color: white;
-    width: 100%;
-    height: 100%;
+@import url('https://fonts.googleapis.com/css2?family=Lobster&display=swap');
+
+.root {
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
+    background-color: #22c3aa;
+}
+
+#avatar {
+    border: 10px solid #aee2d9;
+    border-radius: 50%;
+}
+
+.name {
+    font-family: 'Lobster', cursive;
+    font-size: 2.5rem;
+    color: #ffffff;
+    margin-top: 10px;
+}
+
+.subTitle {
+    color: #aee2d9;
+    font-size: 1.2rem;
+    padding: 0 2rem;
+    text-align: center;
+    /* margin-top: 10px; */
 }
 </style>
