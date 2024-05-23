2115a200eac6ffe08b18cbbd6504a858a379d582
CN-Scars <yss2813483011xxl@gmail.com>
2024-05-23 22:59:24 +0800

    Set up GitHub Actions for deployment

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
+    transpileDependencies: true,
 
-  pluginOptions: {
-    vuetify: {
-			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-		}
-  }
+    pluginOptions: {
+        vuetify: {
+            // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+        }
+    },
+
+    publicPath: process.env.NODE_ENV === 'production' ? '/scars_bio/' : '/',
+    outputDir: 'dist'
 })
